bf86c36e860205a560d30ca063de99132ac16c96
seth-roychan <54307835+seth-roychan@users.noreply.github.com>
2019-12-10 18:12:54 +0800

    Modified Sidebar

--- src/app/core/services/menu-data.service.ts
+++ src/app/core/services/menu-data.service.ts
@@ -15,52 +15,77 @@ export class MenuDataService {
     getMenuList(): CustomMenuItem[] {
         return [
             {
-                Label: 'Home', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
+              Label: '統計訊息', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
             },
             {
-              Label: 'Invoice', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
+              Label: '銷售管理', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
             },
             {
-              Label: 'Setting', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
+              Label: '採購管理', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
             },
             {
-              Label: 'Report', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
+              Label: '庫存管理', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
             },
             {
-              Label: 'Purchase', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
+              Label: '結算賬目', Icon: 'fa-home', RouterLink: null, Childs: [
+                { Label: '應收沖賬', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '應收賬分析', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '應付沖賬', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '應付賬分析', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '收據管理', RouterLink: null, Childs: null, IsChildVisible: false }
+              ], IsChildVisible: false
             },
             {
-              Label: 'Inventory', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
+              Label: '報表分析', Icon: 'fa-home', RouterLink: null, Childs: [
+                { Label: '銷售分析', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '採購分析', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '庫存分析', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '盈利分析', RouterLink: null, Childs: null, IsChildVisible: false },
+                { Label: '客戶分析', RouterLink: null, Childs: null, IsChildVisible: false }
+              ], IsChildVisible: false
             },
             {
-              Label: 'Account', Icon: 'fa-home', RouterLink: '/main/dashboard', Childs: null, IsChildVisible: false
+              Label: '基本設定', Icon: 'fa-home', RouterLink: null, Childs: [
+                { Label: '客戶設定', RouterLink: null, Childs: [
+                  { Label: '客戶類別', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: '客戶設定', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: '付款方式', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: '區域設定', RouterLink: null, Childs: null, IsChildVisible: false }
+                  ], IsChildVisible: false
+                },
+                { Label: '供應商設定', Icon: 'fa-home', RouterLink: null, Childs: [
+                  { Label: 'Menu Level', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: 'Menu Level 1.2.2', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: '客戶設定', RouterLink: null, Childs: null, IsChildVisible: false }
+                  ], IsChildVisible: false
+                },
+                { Label: '用戶設定', Icon: 'fa-cart-plus', RouterLink: null, Childs: [
+                  { Label: 'Menu Level 1.1', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: 'Menu Level 1.2', RouterLink: null, Childs: null, IsChildVisible: false }
+                  ], IsChildVisible: false
+                },
+                { Label: '貨物設定', Icon: 'fa-cart-plus', RouterLink: null, Childs: [
+                  { Label: 'Menu Level 1.1', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: 'Menu Level 1.2', RouterLink: null, Childs: null, IsChildVisible: false }
+                  ], IsChildVisible: false
+                },
+                { Label: '內容設定', Icon: 'fa-cart-plus', RouterLink: null , Childs: [
+                  { Label: 'Menu Level 1.1', RouterLink: null, Childs: null, IsChildVisible: false },
+                  { Label: 'Menu Level 1.2', RouterLink: null, Childs: null, IsChildVisible: false }
+                  ], IsChildVisible:false
+                },
+              ], IsChildVisible: false
             },
             {
-                Label: 'Employees', Icon: 'fa-users', RouterLink: '/main/employees', Childs: null, IsChildVisible: false
-            },
-            {
-                Label: 'Departments', Icon: 'fa-sitemap', RouterLink: '/main/departments', Childs: null, IsChildVisible: false
+              Label: '進階功能', Icon: 'fa-users', RouterLink: '/main/employees', Childs: [
+                { Label: 'Departments', Icon: 'fa-sitemap', RouterLink: '/main/departments', Childs: null, IsChildVisible: false },
+                { Label: 'ContactUs', Icon: 'fa-envelope', RouterLink: '/main/contactus', Childs: null, IsChildVisible: false },
+                { Label: 'Error 404', Icon: 'fa-exclamation-triangle', RouterLink: '/error', Childs: null, IsChildVisible: false }
+              ], IsChildVisible: false
             },
             {
                 Label: 'AboutUs', Icon: 'fa-info-circle', RouterLink: '/main/aboutus', Childs: null, IsChildVisible: false
             },
-            {
-                Label: 'ContactUs', Icon: 'fa-envelope', RouterLink: '/main/contactus', Childs: null, IsChildVisible: false
-            },
-            {
-                Label: 'Error 404', Icon: 'fa-exclamation-triangle', RouterLink: '/error', Childs: null, IsChildVisible: false
-            },
-            {
-                Label: 'Menu Level 1', Icon: 'fa-cart-plus', RouterLink: null, Childs: [
-                    { Label: 'Menu Level 1.1', RouterLink: null, Childs: null, IsChildVisible: false },
-                    {
-                        Label: 'Menu Level 1.2', RouterLink: null, IsChildVisible: false, Childs: [
-                            { Label: 'Menu Level 1.2.1', RouterLink: null, Childs: null, IsChildVisible: false },
-                            { Label: 'Menu Level 1.2.2', RouterLink: null, Childs: null, IsChildVisible: false }
-                        ]
-                    }
-                ], IsChildVisible: false
-            }
         ];
     }
 }
